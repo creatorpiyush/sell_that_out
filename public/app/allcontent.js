082b20ffaded93c35f8576a4b6d26151d3859418
Piyush Anand <piyushanand.1221@gmail.com>
2020-06-02 02:10:09 +0530

    cards fixed

--- public/app/allcontent.js
+++ public/app/allcontent.js
@@ -1,25 +1,24 @@
-function loadPosts() {
+function loadProducts() {
     $.get('/content', (product) => {
         for (let p of product) {
             $('#posts-container').append($(`
-<div class="col-4">
-    <div class="card m-2">
-        <img src="../../${p.itemimg}" class="card-img-top">
-        <div class="card-body">
-            <h5>${p.itemname}</h5>
-            <h5>SPECIAL PRICE</h5>
-            <h5 class="card-subtitle mb-2 text-muted">RS. ${p.price}</h5>
-            <label>Contact details</label>
-            <p class="card-text text-muted">
+    
+    <div class="card-body">
+        <img style="width: 17.25rem; height: 17.5rem;" class="card-img-top" src="../../${p.itemimg}" alt="Card image cap">
+
+        <b><u><h3 class="card-title">${p.itemname}</h3></u></b>
+        <h5>SPECIAL PRICE</h5>
+        <h5 class="card-subtitle mb-2 text-muted">RS. ${p.price}/-</h5>
+        <label>Contact details of Seller</label>
+        <p class="card-text text-muted">
                 ${p.contact}
             </p>
-            
-        </div>
-        <center><a href="#" class="card-link"><input type="button" value="BUY"></a><br><br></center>
-        <center><a href="#" class="card-link"><input type="button" value="ADD TO CART"></a><br><br></center>
+        <center>
+            <a href="#" class="btn btn-primary">BUY This For ${p.price}</a><br><br>
+            <a href="#" class="btn btn-secondary">ADD TO CART</a><br>
+        </center>
     </div>
-</div>
-
+    
 `))
         }
     })
