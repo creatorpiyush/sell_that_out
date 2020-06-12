4e451ec976ef04891c52e3f61ecd9dccd340a01f
Creator Piyush <piyushanand.1221@gmail.com>
2020-06-12 12:30:33 +0530

    db content updated

--- public/app/allcontent.js
+++ public/app/allcontent.js
@@ -3,23 +3,25 @@ function loadProducts() {
     for (let p of product) {
       $("#posts-container").append(
         $(`
-    
-    <div class="card-body">
-        <img style="width: 17.25rem; height: 17.5rem;" class="card-img-top" src="../../${p.itemimg}" alt="Card image cap">
+        <div class="col-4">
+          <div class="card m-2">
+            <div class="card-body">
+                <img style="width: 17.25rem; height: 17.5rem;" class="card-img-top" src="../../${p.itemimg}" alt="Card image cap">
 
-        <b><u><h3 class="card-title">${p.itemname}</h3></u></b>
-        <h5>SPECIAL PRICE</h5>
-        <h5 class="card-subtitle mb-2 text-muted">RS. ${p.price}/-</h5>
-        <label>Contact details of Seller</label>
-        <p class="card-text text-muted">
-                ${p.contact}
-            </p>
-        <center>
-            <a href="#" class="btn btn-primary">BUY This For ${p.price}</a><br><br>
-            <a href="#" class="btn btn-secondary">ADD TO CART</a><br>
-        </center>
-    </div>
-    
+                <b><u><h3 class="card-title">${p.itemname}</h3></u></b>
+                <h5>SPECIAL PRICE</h5>
+                <h5 class="card-subtitle mb-2 text-muted">RS. ${p.price}/-</h5>
+                <label>Contact details of Seller</label>
+                <p class="card-text text-muted">
+                        ${p.contact}
+                    </p>
+                <center>
+                    <a href="#" class="btn btn-primary">BUY This For ${p.price}</a><br><br>
+                    <a href="#" class="btn btn-secondary">ADD TO CART</a><br>
+                </center>
+            </div>
+          </div>
+        </div>    
 `)
       );
     }
