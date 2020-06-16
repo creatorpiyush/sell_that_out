b9d76b49ec9b60e5231e67546fc5c14edf9f6fd5
Creator Piyush <piyushanand.1221@gmail.com>
2020-06-17 00:05:18 +0530

    Some changes in userauth

--- public/app/allcontent.js
+++ public/app/allcontent.js
@@ -3,25 +3,27 @@ function loadProducts() {
     for (let p of product) {
       $("#posts-container").append(
         $(`
-        <div class="col-4">
-          <div class="card m-2">
-            <div class="card-body">
-                <img style="width: 17.25rem; height: 17.5rem;" class="card-img-top" src="../../${p.itemimg}" alt="Card image cap">
-
-                <b><u><h3 class="card-title">${p.itemname}</h3></u></b>
-                <h5>SPECIAL PRICE</h5>
-                <h5 class="card-subtitle mb-2 text-muted">RS. ${p.price}/-</h5>
-                <label>Contact details of Seller</label>
-                <p class="card-text text-muted">
-                        ${p.contact}
-                    </p>
-                <center>
+        
+          <div class="card">
+              <img class="card-img-top" id="images" src="../../${p.itemimg}" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">${p.itemname}</h5>
+            <p class="card-text">
+              ${p.contact}
+            </p>
+            <center>
                     <a href="#" class="btn btn-primary">BUY This For ${p.price}</a><br><br>
                     <a href="#" class="btn btn-secondary">ADD TO CART</a><br>
                 </center>
-            </div>
           </div>
-        </div>    
+          
+
+        <style>
+        #images{
+          height:300px;
+          width: 100%;
+        }
+      </style>
 `)
       );
     }
